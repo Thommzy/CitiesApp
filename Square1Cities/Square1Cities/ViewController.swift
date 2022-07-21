3804a53a95f2fd5ea77edbd24c9ff653b57f9335
Timothy <tomiwaobeisun@gmail.com>
2022-07-21 11:49:50 +0100

    New Xcode Proj Created

---
+++ Square1Cities/Square1Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Square1Cities
+//
+//  Created by Timothy Obeisun on 7/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
